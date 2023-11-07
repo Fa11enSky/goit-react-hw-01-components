3290ecf6a5c57a97b167e196e6cf25578e1ceb13
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2023-11-07 23:24:02 +0200

    some fix

--- src/components/friendList/friendList.jsx
+++ src/components/friendList/friendList.jsx
@@ -2,10 +2,9 @@ import css from './friendList.module.css';
 import FriendListItem from 'components/friendListItem/friendListItem';
 
 const FriendList = ({ friends }) => {
-  return (
-    <ul className={css.friend_list}>
-      <FriendListItem friends={friends} />
-    </ul>
-  );
+  const items = friends.map(friend => {
+    return <FriendListItem key={friend.id} {...friend} />;
+  });
+  return <ul className={css.friend_list}>{items}</ul>;
 };
 export default FriendList;
--- src/components/friendListItem/friendListItem.jsx
+++ src/components/friendListItem/friendListItem.jsx
@@ -1,23 +1,16 @@
 import clsx from 'clsx';
 import css from './index.module.css';
 
-const FriendListItem = ({ friends }) => {
-  return friends.map(el => {
-    return (
-      <li className={css.item} key={el.id}>
-        <span
-          className={clsx(css.status, el.isOnline ? css.online : css.offline)}
-        ></span>
-        <img
-          className={css.avatar}
-          src={el.avatar}
-          alt="User avatar"
-          width="48"
-        />
-        <p className={css.name}>{el.name}</p>
-      </li>
-    );
-  });
+const FriendListItem = ({ isOnline, avatar, name }) => {
+  return (
+    <li className={css.item}>
+      <span
+        className={clsx(css.status, isOnline ? css.online : css.offline)}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
 };
 
 export default FriendListItem;
